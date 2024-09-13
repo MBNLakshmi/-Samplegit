2b9833e38e6706740e304353278fa716f211c4f0
Lucky <lakshmikrishna.narra@gmail.com>
2024-09-13 16:13:30 +0530

    Lakshmi

---
+++ src/test/java/sample/selenium.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+@Test
+
+public class selenium {
+
+	public void testng() {
+		System.out.println("TestNG Project");
+	}
+	public void cucumber() {
+		System.out.println("cucumber project");
+	}
+}
